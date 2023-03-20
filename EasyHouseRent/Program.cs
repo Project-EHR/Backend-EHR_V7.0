06799231162e21037a9d40d84a78c7df87b60482
Juan Pablo Patiño <82609508+JuanFlorez1326@users.noreply.github.com>
2023-03-19 22:35:36 -0500

    ADD: Cors added

--- EasyHouseRent/Program.cs
+++ EasyHouseRent/Program.cs
@@ -1,15 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors( options => {
+    options.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.AllowAnyOrigin();
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,6 +21,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseAuthorization();
 
